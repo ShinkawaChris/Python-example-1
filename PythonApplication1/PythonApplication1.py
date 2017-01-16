4ae070401516b8e41ebc0e16d269ddf43f28e07c
ShinkawaChris <christian69569@aol.com>
2017-01-16 12:40:09 -0500

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,31 @@
+import random
+import sys
+import os
+
+
+print("Hello World")
+
+name = "Dave"
+print(name)
+
+#Numbers Strings Lists Tuples 
+"""nml
+mlm"""
+
+print("(1+2-3)*2 = ", (1 + 2 - 3) * 2)
+
+quote = "\"Always remember you are unique"
+
+multi_line_quote = '''just 
+like everyone else"'''
+# print strings
+new_string = quote + multi_line_quote
+print(new_string)
+
+# examples of placeholders %s
+print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
+print('Hello %s, my name is %s' % ('Mike', 'Chris'))
+
+# print empty lines with \n
+print('\n'* 2)
+
